7958d1f95ae394964de962a0c8ee1e32e5b7b342
fis1995 <fis@stanford.edu>
2021-06-02 21:31:59 -0500

    add EPA to parcel export data

--- storymap/extract_parcels.R
+++ storymap/extract_parcels.R
@@ -3,7 +3,7 @@
 #
 # AUTHOR: Francine Stephens
 # DATE CREATED: 5/23/21
-# LAST UPDATED: 5/23/21
+# LAST UPDATED: 6/2/21
 #-------------------------------------------------------------------------------
 
 ## SETUP------------------------------------------------------------------------
@@ -41,9 +41,12 @@ wgs <- 4326
 setwd("~/Stanford/SOC176/soc176-data-processing/storymap")
 wd <- getwd()
 parceldata_path <- "C:/Users/Franc/Documents/Stanford/landuse_SF"
+epa_parcels <- "/EPA Zoning/"
 
 
 # DATA IMPORTS
+
+## San Francisco
 sf_parcels_shape <- 
   st_read("https://data.sfgov.org/api/geospatial/acdm-wktn?method=export&format=GeoJSON") %>%
   filter(active == "true") %>% 
@@ -66,25 +69,19 @@ sf_tracts <- tracts(state = "CA",
                     progress_bar = F) %>%
   st_transform(wgs)
 
+## EPA Parcel Data
+epa_parcels_shp <- 
+  st_read(paste0(wd, 
+                 epa_parcels,
+                 "Final_Zoning.shp"),
+          quiet = F) 
+
 
 # PREPARE PARCEL DATA FOR EXTRACTION--------------------------------------------
+
+## San Francisco subsets
 sf_subset <- sf_tracts %>%
   filter(TRACTCE %in% sample_tracts)
-
-# View tract subset
-leaflet() %>% 
-  addMapboxTiles(
-    style_id = "streets-v11",
-    username = "mapbox"
-  ) %>% 
-  addPolygons(
-    data = sf_subset,
-    fillColor = "blue",
-    color = "black",
-    weight = 0.5,
-    label = ~TRACTCE
-  )
-
 
 # Link parcels and property data on block lot id
 sf_parcels <- sf_parcels_shape %>% 
@@ -114,24 +111,52 @@ subset_parcels_grouped <- subset_parcels %>%
          zoning_general = as.factor(zone_general)
          )
 
+## City-Center EPA general zoning codes
+epa_parcels_output <- epa_parcels_shp %>% 
+  st_transform(wgs) %>%
+  mutate(gen_zones = case_when(
+    str_detect(GPLU, "Commercial") ~ "Commercial",
+    str_detect(GPLU, "Residential") ~ "Residential",
+    str_detect(GPLU, "Industrial") ~ "Industrial",
+    str_detect(GPLU, "Open Space") ~ "Public Open Space",
+    str_detect(GPLU, "Office") ~ "Office",
+    str_detect(GPLU, "Resource Management") ~ "Conservation Areas"),
+    gen_zones = as.factor(gen_zones)
+    )
 
-## VIEW PARCEL GROUPED MAP------------------------------------------------------
-zoningpal <- colorFactor(palette = c("blue", "red", "green"), 
-                         levels = c("Commercial", "Residential", "Public"))
-
+# View tract subset
 leaflet() %>% 
   addMapboxTiles(
     style_id = "streets-v11",
     username = "mapbox"
   ) %>% 
   addPolygons(
-    data = subset_parcels_grouped %>% st_transform(., wgs),
-    color = ~zoningpal(zoning_general),
+    data = epa_parcels_output,
+    fillColor = "blue",
+    color = "black",
     weight = 0.5,
-    label = ~zoning_general
+    label = ~New_Zone
+  )
+
+
+## VIEW PARCEL GROUPED MAP------------------------------------------------------
+zoningpal <- colorFactor(palette = c("blue", "red", "green", ), 
+                         levels = c("Commercial", "Residential", "Public"))
+zoningpal2 <- colorFactor(topo.colors(6), epa_parcels_output$gen_zones)
+  
+leaflet() %>% 
+  addMapboxTiles(
+    style_id = "streets-v11",
+    username = "mapbox"
+  ) %>% 
+  addPolygons(
+    data = epa_parcels_output %>% st_transform(., wgs),
+    color = ~zoningpal2(gen_zones),
+    weight = 0.5,
+    label = ~gen_zones
   )
 
 
 ## EXPORT PARCELS---------------------------------------------------------------
 st_write(subset_parcels_grouped, "excelsior_parcels_categorized.shp")
-
+st_write(epa_parcels_output, "EPA_parcels_zoned.shp")
